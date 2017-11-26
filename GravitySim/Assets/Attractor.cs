5df48331485ba28f8dc75a4b4c4cb2359cc8afa9
Brian Furman <bfurman777@yahoo.com>
2017-11-25 18:19:00 -0600

    Static List

---
+++ GravitySim/Assets/Attractor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+
+    public static List<Attractor> Attractors;
+    public float G = 1f;
+    public Rigidbody2D rb;
+
+    void FixedUpdate()
+    {
+        foreach (Attractor attractor in Attractors)
+        {
+            if (attractor != this)
+                Attract(attractor);
+        }
+    }
+
+    void OnEnable()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+
+        if (Attractors == null)
+            Attractors = new List<Attractor>();
+
+        Attractors.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Attractors.Remove(this);
+    }
+
+    //add force to everyone but yourself
+    void Attract(Attractor objToAttract)
+    {
+        Rigidbody2D rbToAttract = objToAttract.rb;
+
+        Vector2 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance == 0)
+            return;
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector2 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
---
+++ GravitySim/Assets/CameraMouseFollower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseFollower : MonoBehaviour
+{
+
+    public float dragSpeed = 0.5f;
+    public float zoomSpeed = 10f;
+
+    private Vector3 dragOrigin;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+
+        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+
+
+
+
+        if (!Input.GetMouseButton(0))
+            return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed * -1, pos.y * dragSpeed * -1, 0);
+
+        transform.Translate(move);
+    }
+}
