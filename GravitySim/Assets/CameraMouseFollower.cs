63d5726698025ecd6a5a4e26080abe58fa426c2b
Brian Furman BGHS <bfurman777@yahoo.com>
2017-11-28 11:42:02 -0600

    Better screen dragging

--- GravitySim/Assets/CameraMouseFollower.cs
+++ GravitySim/Assets/CameraMouseFollower.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraMouseFollower : MonoBehaviour
 {
 
-    public float dragSpeed = 0.5f;
+    public float dragSpeed = 0.1f;
     public float zoomSpeed = 10f;
 
     private Vector3 dragOrigin;
@@ -21,7 +21,9 @@ public class CameraMouseFollower : MonoBehaviour
 
 
         Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
-
+		if (Camera.main.orthographicSize <= 0.00001f) {
+			Camera.main.orthographicSize = 0.00001f;
+		}
 
 
 
@@ -29,8 +31,11 @@ public class CameraMouseFollower : MonoBehaviour
         if (!Input.GetMouseButton(0))
             return;
 
+		var cameraScalar = Camera.main.orthographicSize/3;
+
         Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3(pos.x * dragSpeed * -1, pos.y * dragSpeed * -1, 0);
+		Vector3 move = new Vector3(pos.x * dragSpeed * -1 * cameraScalar, 
+			pos.y * dragSpeed * -1 * cameraScalar, 0);
 
         transform.Translate(move);
     }
