f75189b73f5df496a9a7ddfb97edb7ca65841a67
Brian Furman <bfurman777@yahoo.com>
2017-11-28 19:20:49 -0600

    Right click to thrust

--- GravitySim/Assets/CameraMouseFollower.cs
+++ GravitySim/Assets/CameraMouseFollower.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class CameraMouseFollower : MonoBehaviour
 {
 
-    public float dragSpeed = 0.1f;
+    public float dragSpeed = 0.005f;
     public float zoomSpeed = 10f;
 
     private Vector3 dragOrigin;
+    private SpriteRenderer nightSky;
 
+    void Start()
+    {
+        nightSky = this.GetComponentInChildren<SpriteRenderer>();
+        nightSky.enabled = true;
+    }
 
     void Update()
+    {
+        DragScreen();
+        ZoomScreen();
+    }
+
+
+    void DragScreen()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -19,24 +32,37 @@ public class CameraMouseFollower : MonoBehaviour
             return;
         }
 
-
-        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
-		if (Camera.main.orthographicSize <= 0.00001f) {
-			Camera.main.orthographicSize = 0.00001f;
-		}
-
-
-
-
         if (!Input.GetMouseButton(0))
             return;
 
-		var cameraScalar = Camera.main.orthographicSize/3;
-
         Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-		Vector3 move = new Vector3(pos.x * dragSpeed * -1 * cameraScalar, 
-			pos.y * dragSpeed * -1 * cameraScalar, 0);
+		Vector3 move = new Vector3(pos.x * dragSpeed * -1, 
+			pos.y * dragSpeed * -1, 0);
 
         transform.Translate(move);
+    }
+
+    void ZoomScreen()
+    {
+        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+		if (Camera.main.orthographicSize <= 2f) {
+			Camera.main.orthographicSize = 2f;
+		}
+        ResizeSpriteToScreen();
+    }
+
+     void ResizeSpriteToScreen() {
+        if (nightSky == null) return;
+     
+        nightSky.transform.localScale = new Vector3(1,1,1);
+     
+        float width = nightSky.sprite.bounds.size.x;
+        float height = nightSky.sprite.bounds.size.y;
+     
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        nightSky.transform.localScale = new Vector3(worldScreenWidth / width + 1, 
+            worldScreenHeight / height + 1, 1);
     }
 }
--- GravitySim/Assets/GameManagerScript.cs
+++ GravitySim/Assets/GameManagerScript.cs
@@ -4,18 +4,77 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour {
 
-	public static List<Attractor> Attractors;
-	public int phase = 0; //0=place, 1=addForces, 2=actionAndControlCamera
-	//arrowKeys to add force
-	//thrust on your rocket?
-	//online?
-	//timescale?
+    //arrowKeys to add force
+    //thrust on your rocket?
+    //online?
+    //timescale?
+    //rotate planets?
+    //reset round
+    //settings
+    //outOfBounds
+    //toggle button to lock camera on player
+    //speedup or slowdown time
+    //scale thrust exponentially to distance?
 
-	void Start () {
-		Attractors = new List<Attractor>();
-	}
+    public int phase = 1; //1=addForces, 2=actionAndControlCamera
+    public GameObject playerObject;
+    public float distanceToForceConversionScalar = 77;
+
+    private Rigidbody2D playerRB;
+
+    void Start () {
+        Time.timeScale = 0;
+        playerRB = playerObject.GetComponent<Rigidbody2D>();
+        phase = 1;
+    }
 
 	void Update () {
-		
-	}
+		if (phase == 1)
+        {
+            MousePositionInGameCordsAndRotateToward();
+            if (Input.GetMouseButtonDown(1))    //rightClick
+            {
+                var distance = DistanceToMouseFromObject(playerObject);
+                var forceMagnitude = distance * distanceToForceConversionScalar;
+                print("D: " + distance + ", F:" + forceMagnitude);
+                playerRB.AddForce(forceMagnitude * playerObject.transform.up);
+                ChangePhase();
+            }
+        }
+    }
+
+
+    float DistanceToMouseFromObject(GameObject someObject)
+    {
+        var mousePosition = MousePositionInGameCordsAndRotateToward();
+        var objectPosition = someObject.transform.position;
+
+        var distanceVector = mousePosition - objectPosition;
+        return distanceVector.magnitude;
+    }
+
+    Vector3 MousePositionInGameCordsAndRotateToward()
+    {
+        var mousePos = Input.mousePosition;
+        var mouseObjectPos = Camera.main.ScreenToWorldPoint(mousePos);
+        mouseObjectPos.z = 0;
+        LookAt2D(playerObject, mouseObjectPos);
+        return mouseObjectPos;
+    }
+
+    void LookAt2D(GameObject myObject, Vector3 target)
+    {
+        var lookingVector = target - myObject.transform.position;
+        var angle = Mathf.Atan2(lookingVector.y, lookingVector.x) * Mathf.Rad2Deg;
+        myObject.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+    }
+
+    void ChangePhase()
+    {
+        if (phase == 1)
+        {
+            phase = 2;
+            Time.timeScale = 1;
+        }
+    }
 }
